dc923443fe27e93d913779aa60bf3ee0230a9856
Joel Gallant <joelgallant236@gmail.com>
2012-11-23 21:14:04 -0700

    Added buggy stuff

---
+++ src/edu/ata/script/StaticData.java
@@ -0,0 +1,16 @@
+package edu.ata.script;
+
+import java.util.Vector;
+
+public class StaticData {
+
+    private final Object value;
+
+    public StaticData(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
